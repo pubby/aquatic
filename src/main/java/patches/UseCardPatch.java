3a70019d0bdbeee685e089a41883f99207cc3dbf
Alchyr <39511935+Alchyr@users.noreply.github.com>
2023-04-26 04:58:04 -0700

    please do not Prefix SpireReturn important methods
    it makes your mod entirely incompatible with many other mods and breaks parts of basemod which patch the existing method
    
    Instead, use `@ByRef` to change the `monster` variable used by the original method

--- src/main/java/patches/UseCardPatch.java
+++ src/main/java/patches/UseCardPatch.java
@@ -1,8 +1,6 @@
 package aquaticmod.patches;
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
@@ -24,51 +22,13 @@ import java.util.ArrayList;
 )
 public class UseCardPatch
 {
-    public static SpireReturn<Void> Prefix(AbstractPlayer __instance, AbstractCard c, AbstractMonster monster, int energyOnUse)
+    public static void Prefix(AbstractPlayer __instance, AbstractCard c, @ByRef AbstractMonster[] monster, int energyOnUse)
     {
         FrozenField.FrozenDetails d = FrozenField.get(c);
 
-        if ((monster == null)
-        && (d.target == AbstractCard.CardTarget.ENEMY || d.target == AbstractCard.CardTarget.ALL_ENEMY)) {
-            monster = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+        if ((monster[0] == null) && 
+            (d.target == AbstractCard.CardTarget.ENEMY || d.target == AbstractCard.CardTarget.ALL_ENEMY)) {
+            monster[0] = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
         }
-
-        if (c.type == AbstractCard.CardType.ATTACK) {
-            __instance.useFastAttackAnimation();
-        }
-
-        c.calculateCardDamage(monster);
-        if (c.cost == -1 && EnergyPanel.totalCount < energyOnUse && !c.ignoreEnergyOnUse) {
-            c.energyOnUse = EnergyPanel.totalCount;
-        }
-
-        if (c.cost == -1 && c.isInAutoplay) {
-            c.freeToPlayOnce = true;
-        }
-
-        if(!d.frozen) {
-            c.use(__instance, monster);
-        }
-
-
-        AbstractDungeon.actionManager.addToBottom(new UseCardAction(c, monster));
-        if (!c.dontTriggerOnUseCard) {
-            __instance.hand.triggerOnOtherCardPlayed(c);
-        }
-
-        __instance.hand.removeCard(c);
-        __instance.cardInUse = c;
-
-        c.target_x = Settings.WIDTH / 2;
-        c.target_y = Settings.HEIGHT / 2;
-
-        if (!(c.costForTurn <= 0 || c.freeToPlay() || c.isInAutoplay || __instance.hasPower("Corruption") && c.type == AbstractCard.CardType.SKILL)) {
-            __instance.energy.use(c.costForTurn);
-        }
-        if (!__instance.hand.canUseAnyCard() && !__instance.endTurnQueued) {
-            AbstractDungeon.overlayMenu.endTurnButton.isGlowing = true;
-        }
-
-        return SpireReturn.Return(null);
     }
 }
