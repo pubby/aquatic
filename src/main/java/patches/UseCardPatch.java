cdbbeabae9a84591b0e1d4de9820e7ddfc01b069
Alchyr <39511935+Alchyr@users.noreply.github.com>
2023-04-27 07:20:44 -0700

    Update UseCardPatch.java

--- src/main/java/patches/UseCardPatch.java
+++ src/main/java/patches/UseCardPatch.java
@@ -3,13 +3,11 @@ package aquaticmod.patches;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.CorruptionPower;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import aquaticmod.fields.FrozenField;
 import aquaticmod.actions.FreezeCardAction;
@@ -22,21 +20,21 @@ import java.util.ArrayList;
 )
 public class UseCardPatch
 {
-    public static SpireReturn<?> Prefix(AbstractPlayer __instance, AbstractCard c, @ByRef AbstractMonster[] monster, int energyOnUse)
+    public static SpireReturn<?> Prefix(AbstractPlayer __instance, AbstractCard c, AbstractMonster monster, int energyOnUse)
     {
         FrozenField.FrozenDetails d = FrozenField.get(c);
-        
+
         if (d.frozen) {
-            if ((monster[0] == null) && 
+            if ((monster == null) &&
                 (d.target == AbstractCard.CardTarget.ENEMY || d.target == AbstractCard.CardTarget.ALL_ENEMY)) {
-                monster[0] = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
+                monster = AbstractDungeon.getMonsters().getRandomMonster(null, true, AbstractDungeon.cardRandomRng);
             }
 
             c.calculateCardDamage(monster);
             if (c.cost == -1 && EnergyPanel.totalCount < energyOnUse && !c.ignoreEnergyOnUse) {
                 c.energyOnUse = EnergyPanel.totalCount;
             }
-            
+
             if (c.cost == -1 && c.isInAutoplay) {
                 c.freeToPlayOnce = true;
             }
@@ -50,10 +48,10 @@ public class UseCardPatch
             __instance.hand.removeCard(c);
             __instance.cardInUse = c;
 
-            c.target_x = Settings.WIDTH / 2;
-            c.target_y = Settings.HEIGHT / 2;
+            c.target_x = Settings.WIDTH / 2f;
+            c.target_y = Settings.HEIGHT / 2f;
 
-            if (!(c.costForTurn <= 0 || c.freeToPlay() || c.isInAutoplay || __instance.hasPower("Corruption") && c.type == AbstractCard.CardType.SKILL)) {
+            if (!(c.costForTurn <= 0 || c.freeToPlay() || c.isInAutoplay || __instance.hasPower(CorruptionPower.POWER_ID) && c.type == AbstractCard.CardType.SKILL)) {
                 __instance.energy.use(c.costForTurn);
             }
             if (!__instance.hand.canUseAnyCard() && !__instance.endTurnQueued) {
